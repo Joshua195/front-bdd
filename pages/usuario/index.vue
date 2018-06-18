178971f4ce116394e0579c01a99cecbfa2163b7d
Josue Go <gojoshua195@gmail.com>
2018-06-18 03:08:01 -0500

    develop user view

--- pages/usuario/index.vue
+++ pages/usuario/index.vue
@@ -1,19 +1,76 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <v-jumbotron color="grey lighten-2">
-        <v-container fill-height>
-          <v-layout align-center>
-            <v-flex>
-              <h3 class="display-3">Usuario</h3>
-              <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
-              <v-divider class="my-3"></v-divider>
-              <div class="title mb-3">Check out our newest features!</div>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-jumbotron>
+      <div class="text-xs-center">
+        <v-avatar size="200px">
+          <img
+            class="img-circle elevation-7 mb-1"
+            src="/user.png"
+          >
+        </v-avatar>
+        <div class="headline">{{ user.name }} <span style="font-weight:bold">{{ user.firstName }}</span></div>
+        <div class="subheading text-xs-center grey--text pt-1 pb-3">Telefono: {{ user.telephone }}</div>
+        <div class="subheading text-xs-center grey--text pt-1 pb-3">Nombre de usuario: {{ user.username }}</div>
+        <div class="subheading text-xs-center grey--text pt-1 pb-3">
+          <v-btn block @click="openModal()">Modificar datos</v-btn>
+        </div>
+      </div>
     </v-flex>
+    <v-dialog v-model="dialog" persistent max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Perfil de Usuario</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Nombre" v-model="user.name"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  label="Legal last name"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Password" type="password" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age"
+                  required
+                ></v-select>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-select
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                  autocomplete
+                  chips
+                ></v-select>
+              </v-flex>
+            </v-layout>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
@@ -22,7 +79,16 @@
     // middleware: 'auth',
     data: () => {
       return {
-        user: 'user'
+        user: {},
+        dialog: false
+      }
+    },
+    mounted() {
+      this.user = this.$store.state.authUser
+    },
+    methods: {
+      openModal() {
+        this.dialog = true
       }
     }
   }
